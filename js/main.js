d6fdc4fa8803f01e50cc135bedf4c195ca765539
yun <austru00@gmail.com>
2024-12-18 14:54:12 +0900

    Feat: Slide - Image Slide

--- js/main.js
+++ js/main.js
@@ -46,3 +46,21 @@ new Swiper('.notice-line .swiper-container' , {
   autoplay: true,
   loop: true
 })
+
+new Swiper('.promotion .swiper-container',{
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination :{
+    el: '.promotion .swiper-pagination',
+    clickable: true
+  },
+  navigation :{
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+})
