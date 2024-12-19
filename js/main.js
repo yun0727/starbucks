8d3cec28dd74c19fa5439fce835e94b185293b8a
yun <austru00@gmail.com>
2024-12-19 12:27:14 +0900

    Feat: Scroll Position Calculation Animation (ScrollMagic)

--- js/main.js
+++ js/main.js
@@ -76,3 +76,14 @@ promotionToggleBtn.addEventListener('click',function(){
     promotionEl.classList.remove('hide')
   }
 })
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function(spyEl){
+  new ScrollMagic
+  .Scene({ 
+    triggerElement: spyEl, 
+    triggerHook: .8 
+  })
+  .setClassToggle(spyEl, 'show') 
+  .addTo(new ScrollMagic.Controller()) 
+})
